b15814130acd56166bac42dba9cc30e1b7d50be1
Linconl <67241771+LinconlG@users.noreply.github.com>
2025-03-13 14:54:21 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,6 +5,9 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            var calculadora = new Calculadora();
+            var resultado = calculadora.Somar(1, 1);
+            Console.WriteLine(resultado.ToString());
         }
     }
 }
