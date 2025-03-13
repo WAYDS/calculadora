ee4d6fdb5ad9ec3f615417dd83711ecefd2d7bf6
Linconl <67241771+LinconlG@users.noreply.github.com>
2025-03-13 16:30:20 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,14 +2,14 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async void Main(string[] args)
         {
             for (int i = 0; i < 5; i++) {
                 Console.WriteLine("Hello, World!");
                 var calculadora = new Calculadora();
                 var resultado = calculadora.Somar(1, 1);
                 Console.WriteLine(resultado.ToString());
-                Thread.Sleep(1000);
+                await Task.Delay(1000);
             }
 
         }
