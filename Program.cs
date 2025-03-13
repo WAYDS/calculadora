0667b90c20fb77e262eadb8a12b4af04d2f0bb20
Linconl <67241771+LinconlG@users.noreply.github.com>
2025-03-13 16:26:29 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,10 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            var calculadora = new Calculadora();
-            var resultado = calculadora.Somar(1, 1);
-            Console.WriteLine(resultado.ToString());
+            for (int i = 0; i < 5; i++) {
+                Console.WriteLine("Hello, World!");
+                var calculadora = new Calculadora();
+                var resultado = calculadora.Somar(1, 1);
+                Console.WriteLine(resultado.ToString());
+                Thread.Sleep(1000);
+            }
+
         }
     }
 }
