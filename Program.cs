52f2ee24db071e219e5532ff30273267992fae79
Linconl <67241771+LinconlG@users.noreply.github.com>
2025-03-13 16:33:37 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,15 +1,15 @@
 ﻿namespace calculadora
 {
-    internal class Program
+    public class Program
     {
-        static async void Main(string[] args)
+        static void Main(string[] args)
         {
-            for (int i = 0; i < 5; i++) {
+            for (int i = 0; i < 50; i++) {
                 Console.WriteLine("Hello, World!");
                 var calculadora = new Calculadora();
                 var resultado = calculadora.Somar(1, 1);
                 Console.WriteLine(resultado.ToString());
-                await Task.Delay(1000);
+                Thread.Sleep(1000);
             }
 
         }
