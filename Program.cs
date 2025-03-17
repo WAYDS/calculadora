3d1d907161e277cc4527629513c1bbe488322380
Linconl <67241771+LinconlG@users.noreply.github.com>
2025-03-17 14:41:53 -0300

    teste deploy

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             for (int i = 0; i < 1440; i++) {
-                Console.WriteLine("Hello, World!");
+                Console.WriteLine("Hello, World! o deploy automatico deu certoo");
                 var calculadora = new Calculadora();
                 var resultado = calculadora.Somar(1, 1);
                 Console.WriteLine(resultado.ToString());
